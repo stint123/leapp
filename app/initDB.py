3847572ebe037a65b12c70fb231d254b6d2d5831
stint123 <varune23@gmail.com>
2021-03-28 00:01:27 +0000

    init db and created models need to create routes and add javascript

--- app/initDB.py
+++ app/initDB.py
@@ -4,4 +4,15 @@ import json
 
 db.create_all(app=app)
 
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+for row in data:
+    record = Records(updateNum= row['updateNum'], date= row['date'], url= row['url'], 
+    deaths= row['cases']['deaths'], imported= row['cases']['imported'], community= row['cases']['community'], 
+    tested= row['tested'], contact= row['cases']['contact'])
+
+    db.session.add(record)
+
+db.session.commit()
 print('database initialized!')
--- app/models.py
+++ app/models.py
@@ -3,3 +3,24 @@ db = SQLAlchemy()
 
 class Records(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    updateNum = db.Column(db.Integer)
+    date = db.Column(db.Integer)
+    url = db.Column(db.String(200))
+    deaths = db.Column(db.Integer)
+    imported = db.Column(db.Integer)
+    community = db.Column(db.Integer)
+    tested = db.Column(db.Integer)
+    contact = db.Column(db.Integer)
+
+    def toDict(self):
+        return {
+            "id": self.id,
+            "updateNum": self.updateNum,
+            "date": self.date,
+            "url": self.url,
+            "deaths": self.deaths,
+            "imported": self.imported,
+            "community": self.community,
+            "tested": self.tested,
+            "contact":self.contact
+        }
